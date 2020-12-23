e0f5e01f066329333f0f78be6f3e06eb5d0beeb0
Michelle Hoelzer <hoelzermm@gmail.com>
2020-12-23 17:02:08 -0500

    added start button and small interval

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let playSpace = document.getElementById("playSpace");
+let startButton = document.getElementById("start")
 
 let started = false;
 let accurateClick = false;
@@ -6,27 +7,41 @@ let timeLeftToClick = 3; // no time limit to win though
 let sanity = true;
 let clownsNeeded = 10;
 let clownImage = document.createElement("img");
-clownImage.classList.add = "clownImage"
+clownImage.classList.add = "clownImage";
+playSpace.append(clownImage);
 // create setInterval for how long and to make clowns (like while timeleft)
 
-// while (clownsNeeded) {}
-function sadClownEmergence() {
-	let x = Math.floor(Math.random() * 500);
-	let y = Math.floor(Math.random() * 500);
-	// in here have check for if accurate click or not
-	playSpace.append(clownImage)
-	clownImage.src = "images/sadClown.png";
-	clownImage.style.position = "absolute";
-    clownImage.style.left = x + "px";
-	clownImage.style.top = y + "px";
+function startGame() {
+	while (clownsNeeded > 0) {
+		if (clownsNeeded === 10) {
+			sadClownEmergence();
+		} else {
+			setInterval(sadClownEmergence(), 3000);
+		}
+	}
 }
 
-sadClownEmergence()
+function sadClownEmergence() {
+	// if (clownsNeeded === 10) {
+
+	// }
+    let x = Math.floor(Math.random() * 500);
+    let y = Math.floor(Math.random() * 500);
+    // in here have check for if accurate click or not
+    clownImage.src = "images/sadClown.png";
+    clownImage.style.position = "absolute";
+    clownImage.style.left = x + "px";
+    clownImage.style.top = y + "px";
+    clickSuccess();
+}
+// sadClownEmergence();
+
 function clickSuccess() {
     //get happyclown
     // subtract from clownsNeeded
-	// clownsNeeded--;
-	// set small interval before showin sCE
+    clownsNeeded--;
+    console.log(clownsNeeded);
+    // set small interval before showin sCE
     if (clownsNeeded === 0) {
         youWin();
     } else if (timeLeftToClick === 0) {
@@ -34,16 +49,18 @@ function clickSuccess() {
     } else if (timeLeftToClick === 0 && !sanity) {
         finalFailure();
     }
+    // sadClownEmergence()
 }
 
 function youWin() {
     // win condition
+    console.log("ee");
 }
 
 function clickFail() {
-	// youmissed
-	// set small interval before showin sCE
-	sadClownEmergence();
+    // youmissed
+    // set small interval before showin sCE
+    // sadClownEmergence();
 }
 
 function finalFailure() {
@@ -54,15 +71,3 @@ function finalFailure() {
 function resetRound() {
     location.reload();
 }
-
-// let playSpace = document.getElementById("playSpace")
-// let dDButton = document.getElementById("doubleDutyButton")
-
-// let started = false
-
-// if (started) {
-// 	dDButton.textContent = "Hi"
-// 	function doubleDutyButton() {
-// 		location.reload();
-// 	}
-// }
