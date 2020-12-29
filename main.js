74297ad47693a60a64b95c9361b2160f5b479e98
Michelle Hoelzer <hoelzermm@gmail.com>
2020-12-29 10:56:26 -0500

    more stuff; removing unneeded

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 let playSpace = document.getElementById("playSpace");
-let startButton = document.getElementById("start")
+let startButton = document.getElementById("start");
 
 let started = false;
 let accurateClick = false;
-let timeLeftToClick = 3; // no time limit to win though
+let timeToClick = 3; // no time limit to win though
 let sanity = true;
+let sanityPoints = 3; // ["SA", "NI", "TY"]
 let clownsNeeded = 10;
 let clownImage = document.createElement("img");
 clownImage.classList.add = "clownImage";
@@ -12,43 +13,68 @@ playSpace.append(clownImage);
 // create setInterval for how long and to make clowns (like while timeleft)
 
 function startGame() {
-	while (clownsNeeded > 0) {
-		if (clownsNeeded === 10) {
-			sadClownEmergence();
-		} else {
-			setInterval(sadClownEmergence(), 3000);
-		}
-	}
+    started = true;
+    // while (clownsNeeded > 0) {
+    // 	if (clownsNeeded === 10) {
+    sadClownEmergence();
+    // 	} else {
+    // 		setInterval(sadClownEmergence(), 3000);
+    // 	}
+    // }
 }
 
 function sadClownEmergence() {
-	// if (clownsNeeded === 10) {
+    // if (clownsNeeded === 10) {
 
-	// }
-    let x = Math.floor(Math.random() * 500);
-    let y = Math.floor(Math.random() * 500);
-    // in here have check for if accurate click or not
-    clownImage.src = "images/sadClown.png";
-    clownImage.style.position = "absolute";
-    clownImage.style.left = x + "px";
-    clownImage.style.top = y + "px";
-    clickSuccess();
+    // }
+    // while (clownsNeeded > 0) {
+    setInterval(() => {
+        let x = Math.floor(Math.random() * 500);
+        let y = Math.floor(Math.random() * 500);
+        // in here have check for if accurate click or not
+        clownImage.src = "images/sadClown.png";
+        clownImage.style.position = "absolute";
+        clownImage.style.left = x + "px";
+        clownImage.style.top = y + "px";
+        clownImage.addEventListener("click", accuracyCheck);
+        clickStatus();
+        // } else {
+        // 	setInterval(sadClownEmergence(), 3000);
+        // }
+        // }
+    }, 2000);
 }
 // sadClownEmergence();
 
-function clickSuccess() {
-    //get happyclown
+function accuracyCheck(event) {
+    let whatWasClicked = event.currentTarget;
+    // console.log(whatWasClicked === false)
+    // if (whatWasClicked) {
+    accurateClick = true;
+	// }
+	return true
+}
+
+function clickStatus() {
     // subtract from clownsNeeded
-    clownsNeeded--;
     console.log(clownsNeeded);
-    // set small interval before showin sCE
+    // let click =
+    // clownsNeeded--;
     if (clownsNeeded === 0) {
         youWin();
-    } else if (timeLeftToClick === 0) {
+    } else if (accurateClick) {
+    // } else if (timeToClick === 0 && accurateClick) {
+		console.log("eeeee")
+        clickSuccess();
+    } else if (!accurateClick) {
+    // } else if (timeToClick === 0 && !accurateClick) {
         clickFail();
-    } else if (timeLeftToClick === 0 && !sanity) {
-        finalFailure();
-    }
+        // } else if (timeToClick === 0 && !accurateClick && !sanity) {
+        //     finalFailure();
+	}
+	clownImage.src = "images/sadClown.png";
+    // clownsNeeded--;
+    accurateClick = false;
     // sadClownEmergence()
 }
 
@@ -57,15 +83,42 @@ function youWin() {
     console.log("ee");
 }
 
+function clickSuccess() {
+    //get happyclown
+    // set small interval before showin sCE
+    console.log("success");
+    setInterval(() => {
+        clownImage.src = "images/happyClown.png";
+    }, 1000);
+	clownsNeeded--;
+	// accurateClick = false;
+    // sadClownEmergence();
+}
+
 function clickFail() {
     // youmissed
     // set small interval before showin sCE
+    console.log("no");
+
+    setInterval(() => {
+        clownImage.src = "images/youMissed.png";
+    }, 1000);
+    clownsNeeded--;
+    sanityCheck();
     // sadClownEmergence();
 }
 
 function finalFailure() {
     // lostSantiy
     // end game
+}
+
+function sanityCheck() {
+    if (sanityPoints === 0) {
+        finalFailure();
+    } else {
+        sanityPoints--;
+    }
 }
 
 function resetRound() {
